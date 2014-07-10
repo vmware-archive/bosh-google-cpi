ca92cdf6e9cbad78b52a2c91485408ed0c2d4ff7
Ferran Rodenas <frodenas@gmail.com>
2014-07-10 10:08:32 -0700

    Upgrade Google Compute Image packages

--- stemcell_builder/stages/system_google_packages/apply.sh
+++ stemcell_builder/stages/system_google_packages/apply.sh
@@ -11,23 +11,23 @@ source $base_dir/lib/prelude_apply.bash
 mkdir -p $chroot/tmp
 if [ -f $chroot/etc/debian_version ] # Ubuntu
 then
-  cp $assets_dir/google-compute-daemon_1.1.2-1_all.deb $chroot/tmp
-  cp $assets_dir/google-startup-scripts_1.1.2-1_all.deb $chroot/tmp
+  cp $assets_dir/google-compute-daemon_1.1.4-1_all.deb $chroot/tmp
+  cp $assets_dir/google-startup-scripts_1.1.4-1_all.deb $chroot/tmp
 
-  run_in_chroot $chroot "dpkg -i /tmp/google-compute-daemon_1.1.2-1_all.deb /tmp/google-startup-scripts_1.1.2-1_all.deb  || true"
+  run_in_chroot $chroot "dpkg -i /tmp/google-compute-daemon_1.1.4-1_all.deb /tmp/google-startup-scripts_1.1.4-1_all.deb  || true"
   pkg_mgr install
 
-  rm -f /tmp/google-compute-daemon_1.1.2-1_all.deb
-  rm -f /tmp/google-startup-scripts_1.1.2-1_all.deb
+  rm -f /tmp/google-compute-daemon_1.1.4-1_all.deb
+  rm -f /tmp/google-startup-scripts_1.1.4-1_all.deb
 elif [ -f $chroot/etc/centos-release ] # Centos
 then
-  cp $assets_dir/google-compute-daemon-1.1.2-1.noarch.rpm $chroot/tmp
-  cp $assets_dir/google-startup-scripts-1.1.2-1.noarch.rpm $chroot/tmp
+  cp $assets_dir/google-compute-daemon-1.1.4-1.noarch.rpm $chroot/tmp
+  cp $assets_dir/google-startup-scripts-1.1.4-1.noarch.rpm $chroot/tmp
 
-  run_in_chroot $chroot "yum -y install /tmp/google-compute-daemon-1.1.2-1.noarch.rpm /tmp/google-startup-scripts-1.1.2-1.noarch.rpm"
+  run_in_chroot $chroot "yum -y install /tmp/google-compute-daemon-1.1.4-1.noarch.rpm /tmp/google-startup-scripts-1.1.4-1.noarch.rpm"
 
-  rm -f /tmp/google-compute-daemon-1.1.2-1.noarch.rpm
-  rm -f /tmp/google-startup-scripts-1.1.2-1.noarch.rpm
+  rm -f /tmp/google-compute-daemon-1.1.4-1.noarch.rpm
+  rm -f /tmp/google-startup-scripts-1.1.4-1.noarch.rpm
 else
   echo "Unknown OS, exiting"
   exit 2
